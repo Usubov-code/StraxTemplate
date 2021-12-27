20ba574d995313bae9f2c301fcfd717e93e95777
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-27 11:34:57 +0400

    commit cate gory

--- StraxTemplate/Areas/admin/Controllers/CategoryController.cs
+++ StraxTemplate/Areas/admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StraxTemplate.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,14 @@ namespace StraxTemplate.Areas.admin.Controllers
     [Area("admin")]
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Categories.ToList());
         }
     }
 }
