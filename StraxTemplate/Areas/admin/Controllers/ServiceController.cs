366e42f525dbcd1e898fe7ba5dc2d3de0f1753b9
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-28 16:12:16 +0400

    Uploads File (Problem)

--- StraxTemplate/Areas/admin/Controllers/ServiceController.cs
+++ StraxTemplate/Areas/admin/Controllers/ServiceController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StraxTemplate.Data;
 using StraxTemplate.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,10 +15,12 @@ namespace StraxTemplate.Areas.admin.Controllers
     public class ServiceController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ServiceController(AppDbContext context)
+        public ServiceController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -34,8 +38,42 @@ namespace StraxTemplate.Areas.admin.Controllers
         [HttpPost]
         public IActionResult Create(Service model)
         {
-           if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
+                if (model.DesingType.ImageFile.ContentType == "image/jpeg"|| model.DesingType.ImageFile.ContentType == "image/png")
+                {
+                    if (model.DesingType.ImageFile.Length<=3145728)
+                    {
+                        string fileName =Guid.NewGuid()+""+model.DesingType.ImageFile.FileName;
+                        string filePath = Path.Combine(_webHostEnvironment.WebRootPath,"Uploads",fileName);
+
+                        using (var stream = new FileStream(filePath,FileMode.Create)) 
+                        {
+                            model.DesingType.ImageFile.CopyTo(stream);
+                        }
+
+                        model.DesingType.Img = fileName;
+                        model.CreatedTime = DateTime.Now;
+                   
+
+                        _context.Services.Add(model);
+                        _context.SaveChanges();
+                        
+                            return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "you can only upload image file Size 3MB!");
+                        return View(model);
+                    }
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "you can only upload image file!");
+                    return View(model);
+                }
+
                 _context.Services.Add(model);
                 _context.SaveChanges();
 
--- StraxTemplate/Models/DesingType.cs
+++ StraxTemplate/Models/DesingType.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +15,11 @@ namespace StraxTemplate.Models
         public int Id { get; set; }
         [MaxLength(50)]
         public string Img { get; set; }
+        [NotMapped]
+
+        public IFormFile ImageFile { get; set; }
+
+
         public List<Service> Services { get; set; }
     }
 }
